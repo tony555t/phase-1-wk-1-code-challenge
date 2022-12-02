cbe0642f1f6b496372457b6f146867f59559a918
tony555t <tonytullah764@gmail.com>
2022-12-02 15:41:21 +0300

    complete

--- salary.js
+++ salary.js
@@ -1,69 +1,63 @@
-function grossSalary(salary){
-    //payee COMPUTATION
-    if(salary >= 24000) 
-    {
-      console.log(salary * 0.01);}
-
-else if(salary >= 24001 &&  salary <= 32333)
-    {
-      console.log(salary*0.25);}
-else {
-    
-      console.log(salary*0.3);}
-return payee()
-
-}
-///nhif COMPUTATION
-function nhif(salary){
-if(salary >= 5999){ 
-    return(150);
-    }else if(salary >7999){
-    return(300);
-    } else if(salary >11999){
-    return( 500);
-    }else if(salary >14999){
-      return(500);
-    }else if(salary >19999){
-      return( 600);
+let benefits;
+let Salary;
+let NHIF;
+let PAYE;
+let NSSF;
+function salCalc(salary, benefits){
+    let gross = salary + benefits;
+    let NSSF = gross * 0.06;
+    // let netCalculator = gross;
+if (gross <= 24000) {
+        PAYE = (gross*0.1)
     }
-    else if(salary >24999){
-      return(750);
-   } else if(salary >29999){
-    return( 850);
-    }else if(salary >34999){
-      return(900);
-    }else if(salary >39999){
-      return( 950);
+    else if (gross >= 32333) {
+       PAYE = (gross*0.3)
     }
-    else if(salary >44999){
-      return(1000);
-    } else if(salary >49999){
-        return( 1100);
-        }else if(salary >59999){
-          return(1200);
-        }else if(salary >69999){
-          return( 1300);
+    else if (gross >= 24001) {
+        PAYE = (gross*0.25)
+    }
+console.log('Gross salary:', gross)
+console.log('PAYE deductions:', PAYE)
+console.log('NSSF deduction:', NSSF)
+//NHIF COMPUTATION
+        if (gross <= 5999 ){
+           NHIF  = 150 }
+        else if (gross <= 7999) {
+            NHIF = 300
         }
-        else if(salary >79999){
-          return(1400);
-        } 
-        else if(salary >89999){
-            return( 1500);
-            }else if(salary >99999){
-              return(1600);
-            }else 
-              return( 1700);
-            }
-       // nssf COMPUTATION
-       function nssf(salary){
-        if(  salary<= 6000){
-            return(salary* 0.6);
-        }else 
-         return (salary*0.6);
-        
-       } 
-       let taxableincome = grossSalary()-(nssf()+nhif())
-       let netsalary=taxableincome-Payee()
-        return salary
-       
-    
+        else if( gross <= 11999){
+            NHIF =  400 }
+        else if ( gross <= 14999) {
+            NHIF = 500 }
+        else if (gross <= 19999) {
+            NHIF = 600 }
+        else if(gross <= 24999) {
+            NHIF =  750}
+        else if( gross <= 29999){
+            NHIF = 850}
+        else if (gross <= 34999) {
+            NHIF = 900 }
+        else if (gross <= 39999) {
+            NHIF = 950 }
+            else if (gross <= 44999) {
+                NHIF = 1000 }
+            else if (gross <= 49999) {
+                NHIF =  1100 }
+            else if  (gross <= 59999){
+                NHIF = 1200 }
+            else if (gross <= 69999) {
+                NHIF = 1300 }
+            else if (gross <= 79999) {
+                NHIF = 1400 }
+            else if  (gross <= 89999) {
+                NHIF = 1500 }
+                else if (gross <= 99999) {
+                    NHIF = 1600 }
+                else if (gross >= 100000) {
+                    NHIF = 1700 }
+            console.log('NHIF deduction:', NHIF)
+            console.log('Taxes: PAYE + NSSF + NHIF :', (NSSF + PAYE + NHIF ))
+            let netSalary = 'Net Salary:  ' + (gross - (NSSF + PAYE + NHIF ))
+            return netSalary;
+        }
+        console.log(salCalc(40000, 5000));
